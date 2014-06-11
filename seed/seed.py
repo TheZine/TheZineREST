0d714280ceded8b0caa7c08de29d45d4bcc658ed
Abhijeet <void.aby@gmail.com>
2014-06-12 04:09:46 +0530

    added seed.py

---
+++ seed/seed.py
@@ -0,0 +1,52 @@
+
+
+from tz import TZ , Article
+from zine import db,Author,Article,Contact
+
+session = db.session()
+
+def store_contact(author_id ,contact):
+	try:
+		contact = Contact(author_id,contact.email,contact.facebook,contact.twitter,contact.link)
+		session.add(contact)
+	except Exception as e:
+		print e
+		pass
+	finally:
+		print '{0} contact added '.format(author_id)
+			
+
+def store_author(author):
+	try:
+		author = Author(author.id,author.name,author.image)
+		session.add(author)
+	except Exception as e:
+		print e
+		pass
+	finally:
+		print '{0} author added '.format(author.name)
+		store_contact(author.id,author.contact)
+		
+
+def store_article(article):
+	escape_body = 'dummy body'
+	try:
+		article = Article(article.id,article.title,article.tagline,escape_body,article.issue,article.link,article.author.id)
+		session.add(article)
+	except Exception as e:
+		print e
+		pass
+	finally:
+		print '{0} article added '.format(article.id)
+		# store_author(article.author)
+
+for i in range(1,5):		
+	articles  = TZ().get_articles(i)
+	for article in articles:
+		print 'fetching link %s'%article['link']
+		article  = Article.fromLink(article['link'])
+		store_article(article)
+# sql = "SELECT * FROM Article"
+# print Cursor.execute(sql)
+
+session.commit()
